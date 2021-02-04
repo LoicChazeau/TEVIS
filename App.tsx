181ac1cb9348ab9bfba9f1e45650b9ab5a659882
Loic Chazeau <loic.chazeau@epitech.digital>
2021-02-04 17:03:24 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import styles from './style/home';
 export default function App() {
   return (
     <View style={styles.centerRowAligment, styles.test}>
-      <Text>Opeorking your app!</Text>
+      <Text>Opeorking your app!!!!!!!!!!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
