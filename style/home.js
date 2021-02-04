ba59e614cc8c84dd97ef7b09258ff1a0c0999257
Loic Chazeau <loic.chazeau@epitech.digital>
2021-02-04 17:01:52 +0100

    add style js

---
+++ style/home.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    "centerRowAligment":{
+        flex: 1,
+        backgroundColor:"#ffffff",
+        flexDirection: 'row',
+        alignItems:"center"
+    },
+    "centerColumnAligment":{
+        flex: 1,
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    "test":{
+        marginLeft: '40%',
+    }
+
+}
---
+++ style/profile.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    "centerRowAligment":{
+        flex: 1,
+        backgroundColor:"#111111",
+        flexDirection: 'row',
+        alignItems:"center"
+    },
+    "centerColumnAligment":{
+        flex: 1,
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
+
+}
