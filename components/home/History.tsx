c66b092c69e429a4fb3f08b5d07f058947b6d039
Loic Chazeau <loic.chazeau@epitech.digital>
2021-02-05 16:54:03 +0100

    add assets

--- components/home/History.tsx
+++ components/home/History.tsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import {View, Text, StyleSheet, Image} from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 export default function History() {
-  return(
-    <View>
+  return (
+    <View style={{flexDirection: 'row'}}>
+
+      <View style={{ flex: 1 }}>
         <Text style={styles.text}>History</Text>
+      </View>
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Image
+          source={require("../../assets/clock.png")}
+          style={{ width: '30%', height: '30%', flex: 1 }}
+        />
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   text: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     textAlign: "center",
     fontWeight: "bold",
     fontSize: 39,
-  }
+  },
 });
