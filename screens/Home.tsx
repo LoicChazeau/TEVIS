c2c503654ea8cfad7b7e6e32cabc98a70ded3959
Loic <loic.chazeau@epitech.digital>
2021-02-06 18:56:58 +0100

    format style for home screen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity, Dimensions } from "react-native";
-import { StackScreenProps } from '@react-navigation/stack';
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 
 import History from "../components/home/History";
 import Logo from "../components/home/Logo";
@@ -10,85 +9,87 @@ import Settings from "../components/home/Settings";
 import Signin from "../components/home/Signin";
 import Whathot from "../components/home/Whathot";
 
-var { width } = Dimensions.get('window');
-
 export default function Home({ navigation }) {
-  return (
-    <View style={styles.container}>
-
-      <View style={[{ flex: 0.2 }]}></View>
-
-
-      <View style={{ flex: 1.75 }}>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-
-          <View style={{ paddingLeft: 10}}>
-          <Logo />
-          </View>
-
-          <View>
-          <Searchbar />
-          </View>
-
-          <View style={{ justifyContent: 'center', paddingRight: 10}}>
-            <Signin />
-          </View>
-        </View>
-      </View>
-
-      <View style={[{ flex: 2, backgroundColor: "#F96E4F", justifyContent: "center" }]}>
-        <TouchableOpacity onPress={() => navigation.navigate('Login')} style={[{ flex: 1 }]}>
-          <Whathot />
-        </TouchableOpacity>
-      </View>
-
-      <View style={[{ flex: 0.9 }]}></View>
-
-      <View style={[{ flex: 2, backgroundColor: "#EAC652", justifyContent: "center" }]}>
-        <TouchableOpacity style={{ flex: 1 }}>
-          <Newcomment />
-        </TouchableOpacity>
-      </View>
-
-      <View style={[{ flex: 0.9 }]}></View>
-
-      <View style={[{ flex: 2, backgroundColor: "#EAC652", justifyContent: "center" }]}>
-        <TouchableOpacity style={{ flex: 1 }}>
-          <History />
-        </TouchableOpacity>
-      </View>
-
-      <View style={[{ flex: 0.75 }]}></View>
-
-      <View style={{ flex: 1.8, alignItems: 'center' }}>
-        <View style={[{ backgroundColor: "#EAC652", flex: 1, borderTopLeftRadius: 20, borderTopRightRadius: 20, width: "27%" }]}>
-          <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={[{ flex: 1.5 }]}>
-            <Settings />
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={{ flex: 0.2 }}></View>
+			<View style={{ flex: 1.75 }}>
+				<View style={styles.header}>
+					<View style={{ paddingLeft: 10 }}>
+						<Logo />
+					</View>
+					<Searchbar />
+					<View style={styles.signin}>
+						<TouchableOpacity style={{ flex: 1 }}>
+							<Signin />
+						</TouchableOpacity>
+					</View>
+				</View>
+			</View>
+			<View style={styles.whathot}>
+				<TouchableOpacity onPress={() => navigation.navigate("Login")} style={[{ flex: 1 }]}>
+					<Whathot />
+				</TouchableOpacity>
+			</View>
+			<View style={{ flex: 0.9 }}></View>
+			<View style={styles.newcomment}>
+				<TouchableOpacity style={{ flex: 1 }}>
+					<Newcomment />
+				</TouchableOpacity>
+			</View>
+			<View style={[{ flex: 0.9 }]}></View>
+			<View style={styles.history}>
+				<TouchableOpacity style={{ flex: 1 }}>
+					<History />
+				</TouchableOpacity>
+			</View>
+			<View style={[{ flex: 0.75 }]}></View>
+			<View style={{ flex: 1.8, alignItems: "center" }}>
+				<View style={styles.settings}>
+					<TouchableOpacity onPress={() => navigation.navigate("Settings")} style={[{ flex: 1.5 }]}>
+						<Settings />
+					</TouchableOpacity>
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#1A1C1E",
-    justifyContent: 'center',
-  },
-  marginBottom: {
-    marginTop: "15%",
-  },
-  justify: {
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    marginTop: '10%',
-  },
-  text: {
-    color: '#FFFFFF',
-    textAlign: "center",
-    fontWeight: "bold",
-    fontSize: 14,
-  }
+	container: {
+		flex: 1,
+		backgroundColor: "#1A1C1E",
+		justifyContent: "center"
+	},
+	header: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center"
+	},
+	signin: {
+		justifyContent: "center",
+		paddingRight: 10
+	},
+	whathot: {
+		flex: 2,
+		backgroundColor: "#F96E4F",
+		justifyContent: "center"
+	},
+	newcomment: {
+		flex: 2,
+		backgroundColor: "#EAC652",
+		justifyContent: "center"
+	},
+	history: {
+		flex: 2,
+		backgroundColor: "#EAC652",
+		justifyContent: "center"
+	},
+	settings: {
+		backgroundColor: "#EAC652",
+		flex: 1,
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
+		width: "27%"
+	}
 });
