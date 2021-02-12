bf1c0576ae85ac5ac07ff0ce55e040cfe65a3e37
Loic Chazeau <loic.chazeau@epitech.digital>
2021-02-12 10:12:40 +0100

    Update App.js

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ export default function App() {
   return (
     <Provider store={store}>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Whathot" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Settings" component={Settings} />
         <Stack.Screen name="Login" component={Login} />
